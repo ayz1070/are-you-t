02a9bc284bdcf0cd98f9194dd93e9f14c870691f
ayz1070 <ayz1070@gmail.com>
2025-02-25 00:18:28 +0900

    [MemberRepository] 닉네임 중복 검사, 소셜 로그인 중복 검사, 이메일 중복 검사 Repository 구현
    - JPA 적용

--- are_you_t_springboot/src/main/java/study/are_you_t_springboot/repository/MemberRepository.java
+++ are_you_t_springboot/src/main/java/study/are_you_t_springboot/repository/MemberRepository.java
@@ -3,10 +3,15 @@ package study.are_you_t_springboot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.are_you_t_springboot.entity.Member;
+import study.are_you_t_springboot.entity.type.Provider;
 
 import java.util.Optional;
 
+import java.util.UUID;
+
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+    Optional<Member> findByNickname(String nickname);  // 닉네임 중복 검사
+    Optional<Member> findBySocialIdAndProvider(String socialId, Provider provider);  // 소셜 로그인 중복 검사
+    Optional<Member> findByEmail(String email);  // 이메일 중복 검사 (일반 로그인)
 }
