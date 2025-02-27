a339e6eda53c2bbb64ff42fe29fc2a584430e2a3
ayz1070 <ayz1070@gmail.com>
2025-02-28 00:25:23 +0900

    [repository] 사용자 repository 추상 클래스 구현
    - 회원가입
    - 로그인
    - 로그아웃

--- are_you_t_flutter/lib/features/auth/domain/repository/auth_repository.dart
+++ are_you_t_flutter/lib/features/auth/domain/repository/auth_repository.dart
@@ -1,6 +1,17 @@
+import '../../../../core/constants/social_provider.dart';
 import '../entity/user_entity.dart';
 
 abstract class AuthRepository {
-  Future<UserEntity> signUpWithEmail(String email, String password, UserEntity userEntity);
-  Future<UserEntity> signInWithEmail(String email, String password);
+  /// 소셜 계정으로 회원가입
+  Future<UserEntity?> signUpWithSocialAccount({
+    required UserEntity userEntity,
+  });
+
+  /// 소셜 계정으로 로그인
+  Future<UserEntity?> signInWithSocialAccount();
+
+  /// 소셜 계정 로그아웃
+  Future<void> signOut({
+    required SocialProvider provider,
+  });
 }
