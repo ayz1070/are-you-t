36265b9882b6de06202e4ae34b042099686921ba
ayz1070 <ayz1070@gmail.com>
2025-02-28 00:27:11 +0900

    [di] 사용자 domain 클래스 의존성 주입
    - repository
    - usecase

--- are_you_t_flutter/lib/core/di/auth_module.dart
+++ are_you_t_flutter/lib/core/di/auth_module.dart
@@ -1,25 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../features/auth/data/repository_impl/mock/mock_auth_repository_impl.dart';
 import '../../features/auth/domain/repository/auth_repository.dart';
-import '../../features/auth/domain/usecase/sign_in_use_case.dart';
-import '../../features/auth/domain/usecase/sign_up_use_case.dart';
+import '../../features/auth/domain/usecase/sign_in_with_social_account_use_case.dart';
+import '../../features/auth/domain/usecase/sign_up_with_social_account_use_case.dart';
 import '../../features/auth/presentation/state/sign_in_state.dart';
 import '../../features/auth/presentation/state/sign_up_state.dart';
 import '../../features/auth/presentation/viewmodel/sign_in_view_model.dart';
 import '../../features/auth/presentation/viewmodel/sign_up_view_model.dart';
 
-final mockAuthRepositoryProvider = Provider<AuthRepository>((ref) {
-  return MockAuthRepositoryImpl();
-});
 
 // UseCase Providers
-final signUpUseCaseProvider = Provider<SignUpUseCase>((ref) {
-  return SignUpUseCase(ref.read(mockAuthRepositoryProvider));
+final signUpUseCaseProvider = Provider<SignUpWithSocialAccountUseCase>((ref) {
+  return SignUpWithSocialAccountUseCase(ref.read(mockAuthRepositoryProvider));
 });
 
-final signInUseCaseProvider = Provider<SignInUseCase>((ref) {
-  return SignInUseCase(ref.read(mockAuthRepositoryProvider));
+final signInUseCaseProvider = Provider<SignInWithSocialAccountUseCase>((ref) {
+  return SignInWithSocialAccountUseCase(ref.read(mockAuthRepositoryProvider));
 });
 
 // ViewModel Providers
