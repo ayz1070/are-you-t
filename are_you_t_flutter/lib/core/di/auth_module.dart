7787e9a9ddd7c2a41940fcd0c0e2080ee0bd611e
Jun <80628496+ayz1070@users.noreply.github.com>
2025-03-02 16:48:45 +0900

    [fix] ui 구현 전 에러 수정 #22
    -

--- are_you_t_flutter/lib/core/di/auth_module.dart
+++ are_you_t_flutter/lib/core/di/auth_module.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../features/auth/domain/repository/auth_repository.dart';
-import '../../features/auth/domain/usecase/sign_in_use_case.dart';
+import '../../features/auth/domain/usecase/sign_in_with_social_account_use_case.dart';
 import '../../features/auth/domain/usecase/sign_up_with_social_account_use_case.dart';
 import '../../features/auth/presentation/state/sign_in_state.dart';
 import '../../features/auth/presentation/state/sign_up_state.dart';
