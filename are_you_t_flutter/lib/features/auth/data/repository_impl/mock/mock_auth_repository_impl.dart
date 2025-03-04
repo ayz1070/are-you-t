e9fc5592c982e1e3f3ed242e86507aa3412d9b09
ayz1070 <ayz1070@gmail.com>
2025-03-04 23:55:31 +0900

    [mock] mock repository 구현 및 의존성 주입
    -

--- are_you_t_flutter/lib/features/auth/data/repository_impl/mock/mock_auth_repository_impl.dart
+++ are_you_t_flutter/lib/features/auth/data/repository_impl/mock/mock_auth_repository_impl.dart
@@ -1,33 +1,39 @@
 import 'dart:math';
-
+import '../../../../../core/constants/social_provider.dart';
 import '../../../domain/entity/user_entity.dart';
 import '../../../domain/repository/auth_repository.dart';
 
 class MockAuthRepositoryImpl implements AuthRepository {
-  // Mock Data 저장소
+  // Mock Data 저장소 (회원 정보 리스트)
   final List<UserEntity> _mockUserDatabase = [];
 
   @override
-  Future<UserEntity> signUpWithEmail(String email, String password, UserEntity userEntity) async {
-    // 이메일 중복 체크
+  Future<UserEntity?> signUpWithSocialAccount({
+    required UserEntity userEntity,
+  }) async {
+    // 동일한 providerId를 가진 사용자가 이미 존재하는지 확인
     final existingUser = _mockUserDatabase.firstWhere(
-          (user) => user.email == email,
+          (user) =>
+      user.provider == userEntity.provider &&
+          user.providerId == userEntity.providerId,
       orElse: () => UserEntity(
-        userId: '',
+        id: '',
         email: '',
         nickname: '',
         mbti: '',
         profileImage: '',
+        provider: userEntity.provider,
+        providerId: userEntity.providerId,
       ),
     );
 
-    if (existingUser.userId.isNotEmpty) {
-      throw Exception('이미 사용 중인 이메일입니다.');
+    if (existingUser.id.isNotEmpty) {
+      throw Exception('이미 가입된 소셜 계정입니다.');
     }
 
     // Mock userId 생성
     String newUserId = 'user_${Random().nextInt(1000)}';
-    final newUser = userEntity.copyWith(userId: newUserId);
+    final newUser = userEntity.copyWith(id: newUserId);
 
     // Mock Database에 저장
     _mockUserDatabase.add(newUser);
@@ -36,23 +42,17 @@ class MockAuthRepositoryImpl implements AuthRepository {
   }
 
   @override
-  Future<UserEntity> signInWithEmail(String email, String password) async {
-    // 이메일로 사용자 조회
-    final user = _mockUserDatabase.firstWhere(
-          (user) => user.email == email,
-      orElse: () => UserEntity(
-        userId: '',
-        email: '',
-        nickname: '',
-        mbti: '',
-        profileImage: '', id: '', provider: null,
-      ),
-    );
+  Future<UserEntity?> signInWithSocialAccount() async {
+    // 현재 로그인된 사용자 정보가 필요하지만, Mock에서는 랜덤으로 가져옴
+    if (_mockUserDatabase.isEmpty) return null;
 
-    if (user.userId.isEmpty) {
-      throw Exception('이메일 또는 비밀번호가 일치하지 않습니다.');
-    }
+    // 랜덤 사용자 선택 (테스트용)
+    return _mockUserDatabase[Random().nextInt(_mockUserDatabase.length)];
+  }
 
-    return user;
+  @override
+  Future<void> signOut({required SocialProvider provider}) async {
+    // 로그아웃 로직 (Mock에서는 단순히 실행 로그 출력)
+    print('[$provider] 로그아웃 완료');
   }
 }
