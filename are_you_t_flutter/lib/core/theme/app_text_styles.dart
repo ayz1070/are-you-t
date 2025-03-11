ae2232e909adb728de393b1a7407312625ee71dd
ayz1070 <ayz1070@gmail.com>
2025-03-12 00:02:13 +0900

    [core] 공통 스타일 정의 #33
    -

--- are_you_t_flutter/lib/core/theme/app_text_styles.dart
+++ are_you_t_flutter/lib/core/theme/app_text_styles.dart
@@ -6,45 +6,54 @@ class AppTextStyles {
   static const TextStyle headline1 = TextStyle(
     fontSize: 26,
     fontWeight: FontWeight.bold,
-    color: AppColors.textPrimary,
   );
 
   static const TextStyle headline2 = TextStyle(
     fontSize: 22,
     fontWeight: FontWeight.bold,
-    color: AppColors.textPrimary,
   );
 
   static const TextStyle headline3 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
-    color: AppColors.textPrimary,
   );
 
   // 본문 스타일
   static const TextStyle body1 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.normal,
-    color: AppColors.textPrimary,
   );
 
   static const TextStyle body2 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
-    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle body3 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle body4 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.normal,
   );
 
   // 버튼 스타일
   static const TextStyle button = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.bold,
-    color: AppColors.white,
   );
 
   // 캡션 및 보조 텍스트
   static const TextStyle caption = TextStyle(
-    fontSize: 12,
+    fontSize: 8,
     fontWeight: FontWeight.normal,
-    color: AppColors.textSecondary,
+    color: AppColors.textSecondary
+  );
+
+  static const TextStyle overLine = TextStyle(
+      fontSize: 6,
+      fontWeight: FontWeight.normal,
   );
 }
