321c643184b52f6981fa1c8b471d977f1c3aab2c
ayz1070 <ayz1070@gmail.com>
2025-03-17 23:51:53 +0900

    [repository] 회원 repository 구현 #46
    - 회원가입 crud

--- are_you_t_springboot/src/main/java/study/are_you_t_springboot/repository/MemberRepository.java
+++ are_you_t_springboot/src/main/java/study/are_you_t_springboot/repository/MemberRepository.java
@@ -1,17 +1,20 @@
 package study.are_you_t_springboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import study.are_you_t_springboot.entity.Member;
 import study.are_you_t_springboot.entity.type.Provider;
 
 import java.util.Optional;
 
-import java.util.UUID;
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-@Repository
-public interface MemberRepository extends JpaRepository<Member, UUID> {
-    Optional<Member> findByNickname(String nickname);  // 닉네임 중복 검사
-    Optional<Member> findBySocialIdAndProvider(String socialId, Provider provider);  // 소셜 로그인 중복 검사
-    Optional<Member> findByEmail(String email);  // 이메일 중복 검사 (일반 로그인)
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findBySocialIdAndProvider(String socialId, Provider provider);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+
+    Optional<Member> findById(Long id); // 예외 처리를 위한 사용
 }
