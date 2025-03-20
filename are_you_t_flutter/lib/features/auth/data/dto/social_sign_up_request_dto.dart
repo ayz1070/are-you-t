e1b5ab2559787b280ebd8567226649de40f30ffb
ayz1070 <ayz1070@gmail.com>
2025-03-20 23:21:09 +0900

    [dto] 회원 가입 dto 수정 #56
    -

--- are_you_t_flutter/lib/features/auth/data/dto/social_sign_up_request_dto.dart
+++ are_you_t_flutter/lib/features/auth/data/dto/social_sign_up_request_dto.dart
@@ -1,5 +1,8 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../domain/entity/user_entity.dart';
+import '../../../../core/constants/social_provider.dart';
+
 part 'social_sign_up_request_dto.g.dart';
 
 @JsonSerializable()
@@ -18,6 +21,20 @@ class SocialSignUpRequestDto {
     required this.mbti,
   });
 
-  factory SocialSignUpRequestDto.fromJson(Map<String, dynamic> json) => _$SignUpRequestDtoFromJson(json);
-  Map<String, dynamic> toJson() => _$SignUpRequestDtoToJson(this);
+  factory SocialSignUpRequestDto.fromJson(Map<String, dynamic> json) => _$SocialSignUpRequestDtoFromJson(json);
+  Map<String, dynamic> toJson() => _$SocialSignUpRequestDtoToJson(this);
+
+  /// **DTO → Entity 변환**
+  UserEntity toEntity() {
+    return UserEntity(
+      id: socialId,
+      nickname: nickname,
+      mbti: mbti,
+      profileImage: profileImageUrl,
+      provider: SocialProvider.values.firstWhere(
+            (e) => e.name.toUpperCase() == provider.toUpperCase(),
+      ),
+      providerId: socialId,
+    );
+  }
 }
