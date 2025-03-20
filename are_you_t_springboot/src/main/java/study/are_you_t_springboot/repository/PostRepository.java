f3de1258d4d6175c9f8a172b1f1a5d3961967fb6
ayz1070 <ayz1070@gmail.com>
2025-03-21 00:25:28 +0900

    [repository] 게시판 목록에 대한 repository 구현 #59
    -

--- are_you_t_springboot/src/main/java/study/are_you_t_springboot/repository/PostRepository.java
+++ are_you_t_springboot/src/main/java/study/are_you_t_springboot/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package study.are_you_t_springboot.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.are_you_t_springboot.entity.Post;
 
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    // 최신순으로 게시글을 조회하는 메서드 (페이징 적용)
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
